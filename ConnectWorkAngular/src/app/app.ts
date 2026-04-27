806b6f55f672b476c1cd3d07619b093d72a79481
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-26 21:37:52 -0600

    Footer agregado

--- ConnectWorkAngular/src/app/app.ts
+++ ConnectWorkAngular/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./shared/Footer/Footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ConnectWorkAngular');
 }
