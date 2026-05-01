b7f92392641f7dd59ee7c7cb58f825315f87fdb1
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 20:20:14 -0600

    Cambio en el nombre de variable para mayor claridad

--- ConnectWorkRest/src/main/java/models/LoginRequest.java
+++ ConnectWorkRest/src/main/java/models/LoginRequest.java
@@ -1,20 +1,20 @@
 package models;
 
 public class LoginRequest {
-    private String username;
+    private String usernameOrEmail;
     private String password;
 
-    public LoginRequest(String username, String password) {
-        this.username = username;
+    public LoginRequest(String usernameOrEmail, String password) {
+        this.usernameOrEmail = usernameOrEmail;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUsernameOrEmail(String usernameOrEmail) {
+        this.usernameOrEmail = usernameOrEmail;
     }
 
     public String getPassword() {
@@ -24,5 +24,4 @@ public class LoginRequest {
     public void setPassword(String password) {
         this.password = password;
     }
-
 }
