94a783720ef3a48fa2a0eae2a757b9a3c520f760
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 21:50:26 -0600

    Pagina base para la vista principal del cliente

---
+++ ConnectWorkAngular/src/app/pages/FreelancerPrincipal/FreelancerPrincipal.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancer-principal',
+  imports: [],
+  templateUrl: './FreelancerPrincipal.html',
+})
+export class FreelancerPrincipal { }
