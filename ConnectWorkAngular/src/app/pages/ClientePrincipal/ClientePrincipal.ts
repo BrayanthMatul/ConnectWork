03ac64e35b96dbe5a13d357beb5da08d64e1507c
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 21:49:54 -0600

    Pagina base para la vista principal del cliente

---
+++ ConnectWorkAngular/src/app/pages/ClientePrincipal/ClientePrincipal.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-principal',
+  imports: [],
+  templateUrl: './ClientePrincipal.html',
+})
+export class ClientePrincipal { }
