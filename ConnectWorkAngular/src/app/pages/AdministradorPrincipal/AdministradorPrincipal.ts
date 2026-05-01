184bb1b44c05666f328ac4889799997c0a0749c5
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 21:49:14 -0600

    Pagina base para la vistar principal del administrador:

---
+++ ConnectWorkAngular/src/app/pages/AdministradorPrincipal/AdministradorPrincipal.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrador-principal',
+  imports: [],
+  templateUrl: './AdministradorPrincipal.html',
+})
+export class AdministradorPrincipal { }
