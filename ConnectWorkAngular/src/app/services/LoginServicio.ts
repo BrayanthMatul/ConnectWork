22019300e6223f2338232da772829d3474e7c735
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 21:43:43 -0600

    Actualizacon del servicio

--- ConnectWorkAngular/src/app/services/LoginServicio.ts
+++ ConnectWorkAngular/src/app/services/LoginServicio.ts
@@ -1,4 +1,3 @@
-
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -7,54 +6,46 @@ import { Usuario } from '../models/usuario';
 import { environment } from '../../environments/environment';
 
 export interface LoginRequest {
-	username: string;
-	password: string;
+  usernameOrEmail: string;
+  password: string;
 }
 
-
 export interface LoginResponse {
-	token: string;
-	usuario: Usuario;
+  token: string;
+  usuario: Usuario;
 }
 
 @Injectable({ providedIn: 'root' })
 export class LoginServicio {
-    private urlBaseApi = environment.urlBaseApi;
-	private apiUrl = `${this.urlBaseApi}api/login`;
-    private http = inject(HttpClient);
+  private apiUrl = `${environment.urlBaseApi}api/login`;
+  private http = inject(HttpClient);
 
-	login(data: LoginRequest): Observable<LoginResponse> {
-		return this.http.post<LoginResponse>(this.apiUrl, data).pipe(
-			tap(resp => {
-				// Guarda el token en localStorage
-				localStorage.setItem('jwt', resp.token);
-				localStorage.setItem('usuario', JSON.stringify(resp.usuario));
-			}),
-			catchError(this.handleError)
-		);
-	}
+  login(data: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl, data).pipe(
+      tap((resp) => {
+        // Guarda el token en localStorage
+        localStorage.setItem('jwt', resp.token);
+        localStorage.setItem('usuario', JSON.stringify(resp.usuario));
+      }),
+      catchError(this.mostrarError),
+    );
+  }
 
-	logout() {
-		localStorage.removeItem('jwt');
-		localStorage.removeItem('usuario');
-	}
+  logout() {
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('usuario');
+  }
 
-	getToken(): string | null {
-		return localStorage.getItem('jwt');
-	}
+  getToken(): string | null {
+    return localStorage.getItem('jwt');
+  }
 
-	getUsuario(): Usuario | null {
-		const usuario = localStorage.getItem('usuario');
-		return usuario ? JSON.parse(usuario) : null;
-	}
+  getUsuario(): Usuario | null {
+    const usuario = localStorage.getItem('usuario');
+    return usuario ? JSON.parse(usuario) : null;
+  }
 
-	private handleError(error: HttpErrorResponse) {
-		let msg = 'Error desconocido';
-		if (error.error instanceof ErrorEvent) {
-			msg = `Error: ${error.error.message}`;
-		} else if (error.error && error.error.error) {
-			msg = error.error.error;
-		}
-		return throwError(() => msg);
-	}
+  private mostrarError(error: HttpErrorResponse) {
+    return throwError(() => error.error?.error || 'Error desconocido al iniciar sesión');
+  }
 }
