878e23b753671f5ccf648bcf7ae7d134cba9893f
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 21:47:45 -0600

    Servicio del perfil con implementacion de registro de nuevo perfil

---
+++ ConnectWorkAngular/src/app/services/PerfilServicio.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Perfil } from '../models/perfil';
+import { Respuesta } from '../models/respuesta';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PerfilServicio {
+  private url = `${environment.urlBaseApi}api/perfil`;
+  private http = inject(HttpClient);
+
+  registrarPerfil(perfil: Perfil): Observable<Respuesta> {
+    return this.http.post<Respuesta>(this.url, perfil);
+  }
+}
