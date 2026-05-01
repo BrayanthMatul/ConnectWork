1ac09339246da3a7ec6687da45443ae13393fd08
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 19:49:29 -0600

    Clase para enviar respuesta a una peticion

---
+++ ConnectWorkRest/src/main/java/models/Respuesta.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Respuesta {
+    private String clave;
+    private String valor;
+
+    public Respuesta(String clave, String valor) {
+        this.clave = clave;
+        this.valor = valor;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+}
