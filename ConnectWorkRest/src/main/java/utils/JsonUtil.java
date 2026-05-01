9015cefd1580f093262a1df79f5b4417b5095cd4
BrayanthMatul <brayanth.matul146@gmail.com>
2026-04-30 20:23:21 -0600

    Se agrego la clase respuesta para enviar enviar los datos

--- ConnectWorkRest/src/main/java/utils/JsonUtil.java
+++ ConnectWorkRest/src/main/java/utils/JsonUtil.java
@@ -2,14 +2,17 @@ package utils;
 
 import com.google.gson.Gson;
 import jakarta.servlet.http.HttpServletResponse;
+import models.Respuesta;
+
 import java.io.IOException;
 
 public class JsonUtil {
     private static final Gson gson = new Gson();
 
-    public static void escribirJson(HttpServletResponse res, Object data) throws IOException {
+    public static void escribirJson(HttpServletResponse res, String clave, String valor) throws IOException {
         res.setContentType("application/json");
         res.setCharacterEncoding("UTF-8");
-        res.getWriter().write(gson.toJson(data));
+        Respuesta respuesta = new Respuesta(clave, valor);
+        res.getWriter().write(gson.toJson(respuesta));
     }
 }
