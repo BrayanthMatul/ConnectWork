9669f5f0595164be8650cb0c2c777a5b39ab7b74
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-04 13:22:43 -0600

    Implementacion de lista de administradores:

--- ConnectWorkAngular/src/app/components/Administrador/Usuarios/ListaAdministradores/ListaAdministradores.ts
+++ ConnectWorkAngular/src/app/components/Administrador/Usuarios/ListaAdministradores/ListaAdministradores.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Usuario } from '../../../../models/usuario';
+import { UsuarioServicio } from '../../../../services/UsuarioServicio';
+import { TipoUsuario } from '../../../../enums/tipo-usuario';
+import { ModalService } from '../../../../services/ModalService';
 
 @Component({
   selector: 'app-lista-administradores',
   imports: [],
   templateUrl: './ListaAdministradores.html',
 })
-export default class ListaAdministradores {}
+export default class ListaAdministradores {
+  protected administradores = signal<Usuario[]>([]);
+  private usuarioServicio = inject(UsuarioServicio);
+  private modalService = inject(ModalService);
+  protected listaVacia = signal(false);
+
+  ngOnInit() {
+    this.cargarAdministradores();
+  }
+
+  private cargarAdministradores() {
+    this.usuarioServicio.obtenerUsuarios().subscribe({
+      next: (usuarios) => {
+        if (usuarios.length === 0) {
+          this.listaVacia.set(true);
+          return;
+        }
+        this.administradores.set(
+          usuarios.filter((usuario) => usuario.tipoUsuario === TipoUsuario.ADMINISTRADOR),
+        );
+      },
+      error: (error) => {
+        this.modalService.abrirError(
+          'Error al cargar los administradores. Por favor, inténtelo de nuevo más tarde.',
+        );
+      },
+    });
+  }
+}
