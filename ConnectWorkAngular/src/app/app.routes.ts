9f5f56392b8d828c078f3c735505f7bfb8768823
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-03 19:08:01 -0600

    Se agrego la ruta para la vista principal del administrador

--- ConnectWorkAngular/src/app/app.routes.ts
+++ ConnectWorkAngular/src/app/app.routes.ts
@@ -3,8 +3,105 @@ import { Inicio } from './pages/Inicio/Inicio';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'Inicio',
     component: Inicio,
+  },
+
+  {
+    path: 'administrador-principal',
+    loadComponent: () => import('./pages/AdministradorPrincipal/AdministradorPrincipal'),
+    children: [
+      {
+        path: 'registro-administrador',
+        loadComponent: () =>
+          import('./components/Administrador/Usuarios/RegistroAdministrador/RegistroAdministrador'),
+      },
+
+      {
+        path: 'activar-desactivar-usuario',
+        loadComponent: () =>
+          import('./components/Administrador/Usuarios/ActivarDesactivarUsuarios/ActivarDesactivarUsuarios'),
+      },
+
+      {
+        path: 'lista-administradores',
+        loadComponent: () =>
+          import('./components/Administrador/Usuarios/ListaAdministradores/ListaAdministradores'),
+      },
+      {
+        path: 'lista-clientes',
+        loadComponent: () =>
+          import('./components/Administrador/Usuarios/ListadoClientes/ListadoClientes'),
+      },
+      {
+        path: 'lista-freelancers',
+        loadComponent: () =>
+          import('./components/Administrador/Usuarios/ListadoFreelancers/ListadoFreelancers'),
+      },
+
+      {
+        path: 'registro-categoria',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/RegistrarCategoria/RegistrarCategoria'),
+      },
+
+      {
+        path: 'editar-categoria',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/EditarCategoria/EditarCategoria'),
+      },
+      {
+        path: 'activar-desactivar-categoria',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/ActivarDesactivarCategoria/ActivarDesactivarCategoria'),
+      },
+      {
+        path: 'lista-categorias',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/ListaDeCategorias/ListaDeCategorias'),
+      },
+      {
+        path: 'registro-habilidad',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/RegistrarHabilidad/RegistrarHabilidad'),
+      },
+      {
+        path: 'editar-habilidad',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/EditarHabilidad/EditarHabilidad'),
+      },
+      {
+        path: 'desactivar-habilidad',
+        loadComponent: () =>
+          import('./components/Administrador/Categoria/ActivarDesactivarHabilidad/ActivarDesactivarHabilidad'),
+      },
+      {
+        path: 'solicitudes',
+        loadComponent: () => import('./components/Administrador/Categoria/Solicitudes/Solicitudes'),
+      },
+
+      {
+        path: 'saldo-global',
+        loadComponent: () => import('./components/Administrador/Finanzas/SaldoGlobal/SaldoGlobal'),
+      },
+      {
+        path: 'cambio-porcentaje',
+        loadComponent: () =>
+          import('./components/Administrador/Finanzas/CambioPorcentaje/CambioPorcentaje'),
+      },
+
+      {
+        path: 'reportes',
+        loadComponent: () => import('./components/Administrador/Reportes/Reportes'),
+      },
+
+      {
+        path: 'carga-archivo',
+        loadComponent: () => import('./components/Administrador/CargaAchivo/CargaAchivo'),
+      },
+
+      { path: '**', redirectTo: 'registro-administrador' },
+    ],
   },
 
   {
@@ -19,6 +116,6 @@ export const routes: Routes = [
 
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'administrador-principal',
   },
 ];
