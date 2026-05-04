1bdce765213593a427553c62ba38dd632797d42b
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-04 13:24:24 -0600

    Se Implemento el registro de administrador

--- ConnectWorkAngular/src/app/components/Administrador/Usuarios/RegistroAdministrador/RegistroAdministrador.ts
+++ ConnectWorkAngular/src/app/components/Administrador/Usuarios/RegistroAdministrador/RegistroAdministrador.ts
@@ -1,4 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Usuario } from '../../../../models/usuario';
+import { TipoUsuario } from '../../../../enums/tipo-usuario';
+import { UsuarioServicio } from '../../../../services/UsuarioServicio';
+import { ModalService } from '../../../../services/ModalService';
 
 @Component({
   selector: 'app-registro-administrador',
@@ -10,6 +14,36 @@ export default class RegistroAdministrador {
   protected password = signal<string>('');
   protected nombreCompleto = signal<string>('');
   protected email = signal<string>('');
+  private usuarioServicio = inject(UsuarioServicio);
+  private modalService = inject(ModalService);
 
-  protected registrarUsuario() {}
+  protected registrarUsuario() {
+    const nuevoAdministrador: Usuario = {
+      id: 0, // El ID se asignará en el backend
+      username: this.username(),
+      password: this.password(),
+      nombreCompleto: this.nombreCompleto(),
+      email: this.email(),
+      tipoUsuario: TipoUsuario.ADMINISTRADOR,
+    };
+
+    this.usuarioServicio.registrarUsuario(nuevoAdministrador).subscribe({
+      next: (respuesta) => {
+        this.modalService.abrirExito(respuesta.valor);
+        this.limpiarFormulario();
+      },
+      error: (error) => {
+        console.error('Error al registrar el usuario:', error);
+        const mensaje = error.error.valor || 'Error al registrar el usuario';
+        this.modalService.abrirError(mensaje);
+      },
+    });
+  }
+
+  limpiarFormulario() {
+    this.username.set('');
+    this.password.set('');
+    this.nombreCompleto.set('');
+    this.email.set('');
+  }
 }
