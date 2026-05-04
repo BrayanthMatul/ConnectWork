21eab7012c95185a06d4cb20831441b51dfc75c6
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-03 19:09:45 -0600

    Se modifico para no eliminar html y agregar componentes

--- ConnectWorkAngular/src/app/pages/Inicio/Inicio.ts
+++ ConnectWorkAngular/src/app/pages/Inicio/Inicio.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { LoginForm } from '../../components/LoginForm/LoginForm';
 import { Router } from '@angular/router';
+import { Logo } from '../../shared/Logo/Logo';
 
 @Component({
   selector: 'app-inicio',
-  imports: [LoginForm],
+  imports: [LoginForm, Logo],
   templateUrl: './Inicio.html',
   host: {
     class: 'flex-1 flex flex-col w-full',
