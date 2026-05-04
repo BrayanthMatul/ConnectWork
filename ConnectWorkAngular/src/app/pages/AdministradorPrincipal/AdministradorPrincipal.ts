e8b5e19886141518958001f7b32f0f737a27e241
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-03 19:20:52 -0600

    Implementacion de la pagina de admistrador

--- ConnectWorkAngular/src/app/pages/AdministradorPrincipal/AdministradorPrincipal.ts
+++ ConnectWorkAngular/src/app/pages/AdministradorPrincipal/AdministradorPrincipal.ts
@@ -1,8 +1,55 @@
 import { Component } from '@angular/core';
-
+import { MenuOpciones, Menu } from '../../shared/Menu/Menu';
+import { Header } from '../../shared/Header/Header';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-administrador-principal',
-  imports: [],
+  imports: [Header, RouterOutlet],
   templateUrl: './AdministradorPrincipal.html',
+  host: {
+    class: 'flex-1 flex flex-col w-full',
+  },
 })
-export class AdministradorPrincipal { }
+export default class AdministradorPrincipal {
+  protected opcionesMenu = <MenuOpciones[]>[
+    {
+      etiqueta: 'Usuarios',
+      subOpciones: [
+        { etiqueta: 'Registrar administrador', ruta: 'registro-administrador' },
+        { etiqueta: 'Activar/Desactivar Usuario', ruta: 'activar-desactivar-usuario' },
+        { etiqueta: 'Lista de Administradores', ruta: 'lista-administradores' },
+        { etiqueta: 'Lista de Clientes', ruta: 'lista-clientes' },
+        { etiqueta: 'Lista de Freelancers', ruta: 'lista-freelancers' },
+      ],
+    },
+    {
+      etiqueta: 'Categoria',
+      subOpciones: [
+        { etiqueta: 'Registrar Categoria', ruta: 'registro-categoria' },
+        { etiqueta: 'Editar Categoria', ruta: 'editar-categoria' },
+        { etiqueta: 'Activar/Desactivar Categoria', ruta: 'activar-desactivar-categoria' },
+        { etiqueta: 'Lista de Categorias', ruta: 'lista-categorias' },
+        { etiqueta: 'Registrar Habilidad', ruta: 'registro-habilidad' },
+        { etiqueta: 'Editar Habilidad', ruta: 'editar-habilidad' },
+        { etiqueta: 'Desactivar Habilidad', ruta: 'desactivar-habilidad' },
+        { etiqueta: 'Solicitudes', ruta: 'solicitudes' },
+      ],
+    },
+
+    {
+      etiqueta: 'Finanzas',
+      subOpciones: [
+        { etiqueta: 'Saldo global', ruta: 'saldo-global' },
+        { etiqueta: 'Cambio de porcentaje', ruta: 'cambio-porcentaje' },
+      ],
+    },
+    { etiqueta: 'Reportes', ruta: 'reportes' },
+    { etiqueta: 'Cargar Archivos', ruta: 'carga-archivo' },
+  ];
+
+  protected opcionesMenuPerfil = <MenuOpciones[]>[
+    { etiqueta: 'Mi Perfil', ruta: '/' },
+    { etiqueta: 'Editar Perfil', ruta: 'editar-perfil' },
+    { etiqueta: 'Cerrar Sesión', ruta: 'cerrar-sesion' },
+  ];
+}
