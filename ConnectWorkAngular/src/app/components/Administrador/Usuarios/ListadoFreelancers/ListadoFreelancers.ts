374b899e71a5937d2f5fc5d44b66dc02678134ac
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-04 13:21:52 -0600

    Implementacion de listado de freelancer

--- ConnectWorkAngular/src/app/components/Administrador/Usuarios/ListadoFreelancers/ListadoFreelancers.ts
+++ ConnectWorkAngular/src/app/components/Administrador/Usuarios/ListadoFreelancers/ListadoFreelancers.ts
@@ -1,8 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { PerfilServicio } from '../../../../services/PerfilServicio';
+import { Perfil } from '../../../../models/perfil';
+import { TipoUsuario } from '../../../../enums/tipo-usuario';
+import { ListaPerfil } from '../../../../shared/ListaPerfil/ListaPerfil';
 
 @Component({
   selector: 'app-listado-freelancers',
-  imports: [],
+  imports: [ListaPerfil],
   templateUrl: './ListadoFreelancers.html',
 })
-export default class ListadoFreelancers {}
+export default class ListadoFreelancers {
+  private pefilServicio = inject(PerfilServicio);
+  protected perfiles = signal<Perfil[]>([]);
+  protected titulo = signal('Freelancers');
+
+  ngOnInit() {
+    this.cargarPerfiles();
+  }
+
+  private cargarPerfiles() {
+    this.pefilServicio.obtenerPerfiles().subscribe({
+      next: (perfiles) => {
+        this.perfiles.set(
+          perfiles.filter((perfil) => perfil.usuario.tipoUsuario === TipoUsuario.FREELANCER),
+        );
+      },
+      error: (error) => {
+        console.error('Error al cargar los perfiles:', error);
+      },
+    });
+  }
+}
