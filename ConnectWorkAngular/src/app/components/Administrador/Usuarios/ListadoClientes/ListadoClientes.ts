f72b3f712f95974c31e43cb2fefa962b78f57741
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-04 13:21:21 -0600

    Implementacion de listado de clientes

--- ConnectWorkAngular/src/app/components/Administrador/Usuarios/ListadoClientes/ListadoClientes.ts
+++ ConnectWorkAngular/src/app/components/Administrador/Usuarios/ListadoClientes/ListadoClientes.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { PerfilServicio } from '../../../../services/PerfilServicio';
+import { Perfil } from '../../../../models/perfil';
+import { TipoUsuario } from '../../../../enums/tipo-usuario';
+import { ListaPerfil } from '../../../../shared/ListaPerfil/ListaPerfil';
 
 @Component({
   selector: 'app-listado-clientes',
-  imports: [],
+  imports: [ListaPerfil],
   templateUrl: './ListadoClientes.html',
 })
-export default class ListadoClientes {}
+export default class ListadoClientes {
+  private pefilServicio = inject(PerfilServicio);
+  protected perfiles = signal<Perfil[]>([]);
+  protected titulo = signal('Clientes');
+
+  ngOnInit() {
+    this.cargarPerfiles();
+  }
+
+  private cargarPerfiles() {
+    this.pefilServicio.obtenerPerfiles().subscribe({
+      next: (perfiles) => {
+        this.perfiles.set(
+          perfiles.filter((perfil) => perfil.usuario.tipoUsuario === TipoUsuario.CLIENTE),
+        );
+      },
+      error: (error) => {
+        console.error('Error al cargar los perfiles:', error);
+      },
+    });
+  }
+}
