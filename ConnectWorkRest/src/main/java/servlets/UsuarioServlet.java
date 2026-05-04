159fea14d7027f802ac2bed3f8f042765ed76357
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-04 15:17:02 -0600

    Implementacion del servlet de usuarios

--- ConnectWorkRest/src/main/java/servlets/UsuarioServlet.java
+++ ConnectWorkRest/src/main/java/servlets/UsuarioServlet.java
@@ -1,4 +1,67 @@
 package servlets;
 
-public class UsuarioServlet {
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import exceptions.CorreoUsuarioException;
+import exceptions.NombreUsuarioException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.Usuario;
+import services.UsuarioServicio;
+import utils.JsonUtil;
+import utils.PermissionUtil;
+
+@WebServlet(urlPatterns = { "/api/usuario" })
+public class UsuarioServlet extends HttpServlet {
+
+    private final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+    private final UsuarioServicio servicioUsuario = new UsuarioServicio();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        try {
+            if (!PermissionUtil.verificarAdministrador(req, res)) {
+                return;
+            }
+
+            // Si es admin, proceder a crear el nuevo usuario
+            Usuario nuevoUsuario = gson.fromJson(req.getReader(), Usuario.class);
+            servicioUsuario.crearUsuario(nuevoUsuario);
+            res.setStatus(HttpServletResponse.SC_CREATED);
+            JsonUtil.escribirJson(res, "message", "Usuario creado exitosamente");
+        } catch (NombreUsuarioException e) {
+            res.setStatus(HttpServletResponse.SC_CONFLICT);
+            JsonUtil.escribirJson(res, "error", "El nombre de usuario ya existe.");
+        } catch (CorreoUsuarioException e) {
+            res.setStatus(HttpServletResponse.SC_CONFLICT);
+            JsonUtil.escribirJson(res, "error", "El correo electrónico ya está registrado.");
+        } catch (SQLException e) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            JsonUtil.escribirJson(res, "error", "Error al crear el perfil en la base de datos: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        try {
+            if (!PermissionUtil.verificarAdministrador(req, res)) {
+                return;
+            }
+
+            List<Usuario> usuarios = servicioUsuario.obtenerTodosLosUsuarios();
+            res.setContentType("application/json");
+            res.setCharacterEncoding("UTF-8");
+            res.getWriter().write(gson.toJson(usuarios));
+        } catch (SQLException e) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            JsonUtil.escribirJson(res, "error", "Error al obtener los usuarios: " + e.getMessage());
+        }
+    }
 }
