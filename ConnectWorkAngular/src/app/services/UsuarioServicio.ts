8ef85e10b803b694f9f0262762a5a2df7a411d97
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-04 13:13:49 -0600

    Servicio para CRUD de usuarios

---
+++ ConnectWorkAngular/src/app/services/UsuarioServicio.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from '@angular/core';
+import { Respuesta } from '../models/respuesta';
+import { Usuario } from '../models/usuario';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioServicio {
+  private url = `${environment.urlBaseApi}api/usuario`;
+  private http = inject(HttpClient);
+
+  public registrarUsuario(usuario: Usuario): Observable<Respuesta> {
+    return this.http.post<Respuesta>(this.url, usuario);
+  }
+
+  public obtenerUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.url);
+  }
+}
