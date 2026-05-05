aeb1bb3916201a50fa28d0233bfb48871559db74
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-05 12:01:04 -0600

    Se implemento del servicio de categoria

--- ConnectWorkRest/src/main/java/services/CategoriaServicio.java
+++ ConnectWorkRest/src/main/java/services/CategoriaServicio.java
@@ -1,4 +1,41 @@
+
 package services;
 
+import daos.CategoriaDAO;
+import exceptions.NombreRepetidoException;
+import models.Categoria;
+import java.sql.SQLException;
+import java.util.List;
+
 public class CategoriaServicio {
+    private final CategoriaDAO categoriaDAO;
+
+    public CategoriaServicio() {
+        this.categoriaDAO = new CategoriaDAO();
+    }
+
+    public void crearNuevaCategoria(Categoria categoria) throws SQLException, NombreRepetidoException {
+        Categoria existente = categoriaDAO.obtenerPorNombre(categoria.getNombre());
+        if (existente != null) {
+            throw new NombreRepetidoException("Ya existe una categoría con ese nombre.");
+        }
+
+        categoriaDAO.insertarCategoria(categoria);
+    }
+
+    public List<Categoria> obtenerTodasLasCategorias() throws SQLException {
+        return categoriaDAO.obtenerTodas();
+    }
+
+    public void actualizarEstadoCategoria(int id, boolean activo) throws SQLException {
+        categoriaDAO.actualizarEstado(id, activo);
+    }
+
+    public void actualizarCategoria(Categoria categoria) throws SQLException, NombreRepetidoException {
+        Categoria existente = categoriaDAO.obtenerPorNombre(categoria.getNombre());
+        if (existente != null && existente.getId() != categoria.getId()) {
+            throw new NombreRepetidoException("Ya existe una categoría con ese nombre.");
+        }
+        categoriaDAO.actualizarCategoria(categoria);
+    }
 }
