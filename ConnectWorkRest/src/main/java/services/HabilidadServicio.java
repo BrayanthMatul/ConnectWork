c3c7a0410dd776f49fccbd3f3d71a381aef52290
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-05 12:01:29 -0600

    Se implemento del servicio de Habilidad

--- ConnectWorkRest/src/main/java/services/HabilidadServicio.java
+++ ConnectWorkRest/src/main/java/services/HabilidadServicio.java
@@ -1,4 +1,39 @@
 package services;
 
+import daos.HabilidadDAO;
+import exceptions.NombreRepetidoException;
+import models.Habilidad;
+import java.sql.SQLException;
+import java.util.List;
+
 public class HabilidadServicio {
+    private final HabilidadDAO habilidadDAO;
+
+    public HabilidadServicio() {
+        this.habilidadDAO = new HabilidadDAO();
+    }
+
+    public void registrarNuevaHabilidad(Habilidad habilidad) throws SQLException, NombreRepetidoException {
+        Habilidad existente = habilidadDAO.obtenerPorNombre(habilidad.getNombre());
+        if (existente != null) {
+            throw new NombreRepetidoException("Ya existe una habilidad con ese nombre.");
+        }
+        habilidadDAO.insertar(habilidad);
+    }
+
+    public List<Habilidad> obtenerTodasLasHabilidades() throws SQLException {
+        return habilidadDAO.obtenerTodos();
+    }
+
+    public void actualizarEstadoHabilidad(int id, boolean activo) throws SQLException {
+        habilidadDAO.actualizarEstado(id, activo);
+    }
+
+    public void actualizarHabilidad(Habilidad habilidad) throws SQLException, NombreRepetidoException {
+        Habilidad existente = habilidadDAO.obtenerPorNombre(habilidad.getNombre());
+        if (existente != null && existente.getId() != habilidad.getId()) {
+            throw new NombreRepetidoException("Ya existe una habilidad con ese nombre.");
+        }
+        habilidadDAO.actualizar(habilidad);
+    }
 }
