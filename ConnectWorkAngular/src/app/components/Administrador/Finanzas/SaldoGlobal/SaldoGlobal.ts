f5709881f68663d786179fc7b4f7173b60a2f130
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-05 11:49:50 -0600

    Se implemento el componente para ver el saldo global del sistema

--- ConnectWorkAngular/src/app/components/Administrador/Finanzas/SaldoGlobal/SaldoGlobal.ts
+++ ConnectWorkAngular/src/app/components/Administrador/Finanzas/SaldoGlobal/SaldoGlobal.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { SaldoSistemaServicio } from '../../../../services/SaldoSistemaServicio';
 
 @Component({
   selector: 'app-saldo-global',
   imports: [],
   templateUrl: './SaldoGlobal.html',
 })
-export default class SaldoGlobal {}
+export default class SaldoGlobal {
+  protected saldoGlobal = signal<number>(0);
+  private saldoSistemaServicio = inject(SaldoSistemaServicio);
+
+  ngOnInit() {
+    this.cargarSaldoGlobal();
+  }
+
+  private cargarSaldoGlobal() {
+    this.saldoSistemaServicio.obtenerTodosLosRegistros().subscribe({
+      next: (response) => {
+        this.saldoGlobal.set(response.totalSaldo);
+      },
+      error: (error) => {
+        console.error('Error al cargar el saldo global:', error);
+      },
+    });
+  }
+}
