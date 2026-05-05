b60f48faaaae5afd8e39ebc1b8cab270db1d3724
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-05 12:02:07 -0600

    Se implemento del servicio de historial de porcentaje

--- ConnectWorkRest/src/main/java/services/HistorialPorcentajeComisionServicio.java
+++ ConnectWorkRest/src/main/java/services/HistorialPorcentajeComisionServicio.java
@@ -1,4 +1,33 @@
 package services;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import daos.HistorialPorcentajeComisionDAO;
+import models.HistorialPorcentajeComision;
+
 public class HistorialPorcentajeComisionServicio {
+
+    private final HistorialPorcentajeComisionDAO historialComisionDAO;
+
+    public HistorialPorcentajeComisionServicio() {
+        this.historialComisionDAO = new HistorialPorcentajeComisionDAO();
+    }
+
+    public void registrarNuevoHistorialComision(HistorialPorcentajeComision historialComision) throws SQLException {
+        try {
+            historialComisionDAO.insertar(historialComision);
+        } catch (SQLException e) {
+            throw new SQLException("Error al registrar el nuevo historial de comisión: " + e.getMessage(), e);
+        }
+    }
+
+    public List<HistorialPorcentajeComision> obtenerTodosLosHistoriales() throws SQLException {
+        try {
+            return historialComisionDAO.obtenerTodos();
+        } catch (SQLException e) {
+            throw new SQLException("Error al obtener los historiales de comisión: " + e.getMessage(), e);
+        }
+    }
+
 }
