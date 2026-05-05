e0e5b744030a4a7320de45153de7dce5cf40991f
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-05 11:49:07 -0600

    Se implemento el componente para modificar el porcentaje y mostrar el porcentaje actual

--- ConnectWorkAngular/src/app/components/Administrador/Finanzas/CambioPorcentaje/CambioPorcentaje.ts
+++ ConnectWorkAngular/src/app/components/Administrador/Finanzas/CambioPorcentaje/CambioPorcentaje.ts
@@ -1,8 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { HistorialPorcentajeServicio } from '../../../../services/HistorialPorcentajeServicio';
+import { ModalService } from '../../../../services/ModalService';
+import { HistorialPorcentajeComision } from '../../../../models/historial-porcentaje-comision';
 
 @Component({
   selector: 'app-cambio-porcentaje',
   imports: [],
   templateUrl: './CambioPorcentaje.html',
 })
-export default class CambioPorcentaje {}
+export default class CambioPorcentaje {
+  protected nuevoPorcentaje = signal<number>(0);
+  protected porcentajeActual = signal<number>(0);
+  private historialPorcentajeServicio = inject(HistorialPorcentajeServicio);
+  private modalServicio = inject(ModalService);
+
+  ngOnInit() {
+    this.cargarPorcentajeActual();
+  }
+
+  private cargarPorcentajeActual() {
+    this.historialPorcentajeServicio.obtenerUltimoHistorial().subscribe({
+      next: (historial) => {
+        if (historial) {
+          this.porcentajeActual.set(historial.porcentajeComision);
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar el porcentaje actual:', error);
+      },
+    });
+  }
+
+  protected cambiarPorcentaje(): void {
+    if (this.nuevoPorcentaje() < 0 || this.nuevoPorcentaje() > 100) {
+      this.modalServicio.abrirError('El porcentaje debe estar entre 0 y 100.');
+      return;
+    }
+
+    const nuevoHistorial: HistorialPorcentajeComision = {
+      id: 0, // El ID se asignará automáticamente en el backend
+      fechaHoraInicio: new Date(), // Se asignará automáticamente en el backend
+      fechaHoraFin: new Date(), // Se asignará automáticamente en el backend cuando se registre un nuevo historial
+      porcentajeComision: this.nuevoPorcentaje(),
+    };
+
+    this.historialPorcentajeServicio.registrarNuevoHistorial(nuevoHistorial).subscribe({
+      next: (respuesta) => {
+        this.modalServicio.abrirExito(respuesta.valor);
+        this.cargarPorcentajeActual();
+        this.nuevoPorcentaje.set(0);
+      },
+      error: (error) => {
+        console.error('Error al cambiar el porcentaje:', error);
+        this.modalServicio.abrirError(
+          'Hubo un error al cambiar el porcentaje. Por favor, inténtalo de nuevo.',
+        );
+      },
+    });
+  }
+}
