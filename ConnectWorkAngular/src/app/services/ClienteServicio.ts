0168b9b3efd8faed27c057af1179fd760389e186
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 07:03:23 -0600

    Servicio para el cliente

---
+++ ConnectWorkAngular/src/app/services/ClienteServicio.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Cliente } from '../models/cliente';
+import { Respuesta } from '../models/respuesta';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClienteServicio {
+  private url = `${environment.urlBaseApi}api/cliente`;
+  private http = inject(HttpClient);
+
+  public registrarDatosInicialesCliente(cliente: Cliente): Observable<Respuesta> {
+    return this.http.post<Respuesta>(this.url, cliente);
+  }
+}
