ca73ce3af51637dd96414d0f9a67ae19c8789950
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 07:15:07 -0600

    Interfaz inicial

--- ConnectWorkAngular/src/app/pages/ClientePrincipal/ClientePrincipal.ts
+++ ConnectWorkAngular/src/app/pages/ClientePrincipal/ClientePrincipal.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
+import { MenuOpciones } from '../../shared/Menu/Menu';
+import { Header } from '../../shared/Header/Header';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-principal',
-  imports: [],
+  imports: [Header, RouterOutlet],
   templateUrl: './ClientePrincipal.html',
+  host: {
+    class: 'flex-1 flex flex-col w-full',
+  },
 })
-export default class ClientePrincipal {}
+export default class ClientePrincipal {
+  protected opcionesMenu = <MenuOpciones[]>[
+    {
+      etiqueta: 'Proyectos',
+      subOpciones: [
+        { etiqueta: 'Publicar Proyecto', ruta: 'publicar-proyecto' },
+        { etiqueta: 'Cancelar Proyecto', ruta: 'cancelar-proyecto' },
+        { etiqueta: 'Proyectos Publicados', ruta: 'proyectos-publicados' },
+      ],
+    },
+
+    { etiqueta: 'Propuestas Recibidas', ruta: 'propuestas-recibidas' },
+    { etiqueta: 'Entregas Recibidas', ruta: 'entregas-recibidas' },
+
+    {
+      etiqueta: 'Finanzas',
+      subOpciones: [
+        { etiqueta: 'Saldo', ruta: 'saldo' },
+        { etiqueta: 'Recargar Saldo', ruta: 'recargar-saldo' },
+      ],
+    },
+    { etiqueta: 'Reportes', ruta: 'reportes' },
+  ];
+
+  protected opcionesMenuPerfil = <MenuOpciones[]>[
+    { etiqueta: 'Mi Perfil', ruta: '/' },
+    { etiqueta: 'Editar Perfil', ruta: 'editar-perfil' },
+  ];
+}
