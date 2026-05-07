5ce475fdb22bfa413daa24cd66dea3c40aea394a
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 07:20:19 -0600

    Implementar el método registrarNuevoFreelancer en FreelancerDAO para agregar nuevos freelancers a la base de datos

--- ConnectWorkRest/src/main/java/daos/FreelancerDAO.java
+++ ConnectWorkRest/src/main/java/daos/FreelancerDAO.java
@@ -1,4 +1,29 @@
 package daos;
 
+import database.ConexionDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import models.Freelancer;
+
 public class FreelancerDAO {
+
+    public void registrarNuevoFreelancer(Freelancer freelancerNuevo) throws SQLException {
+        String query = "INSERT INTO freelancers (id_freelancer, biografia, nivel_experiencia, calificacion, tarifa_hora) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = ConexionDB.getConexion();
+                PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setInt(1, freelancerNuevo.getIdFreelancer());
+            ps.setString(2, freelancerNuevo.getBiografia());
+            ps.setString(3, freelancerNuevo.getNivelExperiencia());
+            ps.setFloat(4, freelancerNuevo.getCalificacion());
+            ps.setBigDecimal(5, freelancerNuevo.getTarifaHora());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("Error al registrar el nuevo freelancer: " + e.getMessage(), e);
+        }
+
+        PerfilDAO perfilDAO = new PerfilDAO();
+        perfilDAO.marcarPerfilComoCompleto(freelancerNuevo.getIdFreelancer());
+    }
 }
