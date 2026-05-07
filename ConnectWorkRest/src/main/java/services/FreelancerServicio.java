4f7a88773417770948b53aedbd9cfa4b9228cb95
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 07:22:56 -0600

    Agregar clase FreelancerServicio para gestionar el registro de nuevos freelancers

--- ConnectWorkRest/src/main/java/services/FreelancerServicio.java
+++ ConnectWorkRest/src/main/java/services/FreelancerServicio.java
@@ -1,4 +1,24 @@
 package services;
 
+import java.sql.SQLException;
+
+import daos.FreelancerDAO;
+import models.Freelancer;
+
 public class FreelancerServicio {
+
+    private final FreelancerDAO freelancerDAO;
+
+    public FreelancerServicio() {
+        this.freelancerDAO = new FreelancerDAO();
+    }
+
+    public void registrarNuevoFreelancer(Freelancer freelancer) throws SQLException {
+        try {
+            freelancerDAO.registrarNuevoFreelancer(freelancer);
+        } catch (SQLException e) {
+            throw new SQLException("Error al registrar el nuevo freelancer: " + e.getMessage(), e);
+        }
+    }
+
 }
