3e05d6ad3031fdc7dcbf5558ef360b9a34346366
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 07:22:26 -0600

    Agregar clase ClienteServicio para gestionar el registro de nuevos clientes

--- ConnectWorkRest/src/main/java/services/ClienteServicio.java
+++ ConnectWorkRest/src/main/java/services/ClienteServicio.java
@@ -1,4 +1,23 @@
 package services;
 
+import java.sql.SQLException;
+
+import daos.ClienteDAO;
+import models.Cliente;
+
 public class ClienteServicio {
+
+    private final ClienteDAO clienteDAO;
+
+    public ClienteServicio() {
+        this.clienteDAO = new ClienteDAO();
+    }
+
+    public void registrarNuevoCliente(Cliente cliente) throws SQLException {
+        try {
+            clienteDAO.registrarNuevoCliente(cliente);
+        } catch (SQLException e) {
+            throw new SQLException("Error al registrar el nuevo cliente: " + e.getMessage(), e);
+        }
+    }
 }
