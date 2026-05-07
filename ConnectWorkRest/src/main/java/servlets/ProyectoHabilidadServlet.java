8307b4761f65879bd86fac8a64e9b1a31d4a21fc
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 07:19:34 -0600

    Implementar la clase ProyectoHabilidadServlet con el método doGet para manejar solicitudes de habilidades de proyectos

--- ConnectWorkRest/src/main/java/servlets/ProyectoHabilidadServlet.java
+++ ConnectWorkRest/src/main/java/servlets/ProyectoHabilidadServlet.java
@@ -1,4 +1,36 @@
 package servlets;
 
-public class ProyectoHabilidadServlet {
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import services.ProyectoHabilidadServicio;
+import utils.JsonUtil;
+import utils.PermissionUtil;
+
+@WebServlet(urlPatterns = { "/api/proyecto-habilidad" })
+public class ProyectoHabilidadServlet extends HttpServlet {
+    private final Gson gson = new Gson();
+    private final ProyectoHabilidadServicio proyectoHabilidadServicio = new ProyectoHabilidadServicio();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        try {
+            if (!PermissionUtil.verificarAutenticacion(req, res)) {
+                return;
+            }
+
+            res.setStatus(HttpServletResponse.SC_OK);
+            res.setContentType("application/json");
+            gson.toJson(proyectoHabilidadServicio.obtenerTodo(), res.getWriter());
+        } catch (SQLException e) {
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            JsonUtil.escribirJson(res, "error", "Error al obtener las habilidades de los proyectos: " + e.getMessage());
+        }
+    }
 }
