41ebe01a24f4f1762f623b7ccfbd1b70e77dfa9d
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 21:41:34 -0600

    Agregar componente SolicitarHabilidad con su plantilla HTML

---
+++ ConnectWorkAngular/src/app/components/Freelancer/SolicitarHabilidad/SolicitarHabilidad.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitar-habilidad',
+  imports: [],
+  templateUrl: './SolicitarHabilidad.html',
+})
+export default class SolicitarHabilidad {}
