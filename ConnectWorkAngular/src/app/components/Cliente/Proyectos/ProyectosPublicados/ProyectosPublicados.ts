8e4f668be18b66cf97216157fd40e9911825c227
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 18:11:07 -0600

    Implementar la funcionalidad para mostrar proyectos publicados y activos, incluyendo carga de datos y manejo de errores

--- ConnectWorkAngular/src/app/components/Cliente/Proyectos/ProyectosPublicados/ProyectosPublicados.ts
+++ ConnectWorkAngular/src/app/components/Cliente/Proyectos/ProyectosPublicados/ProyectosPublicados.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProyectoServicio } from '../../../../services/ProyectoServicio';
+import { Proyecto } from '../../../../models/proyecto';
+import { LoginServicio } from '../../../../services/LoginServicio';
+import { ModalService } from '../../../../services/ModalService';
 
 @Component({
   selector: 'app-proyectos-publicados',
   imports: [],
   templateUrl: './ProyectosPublicados.html',
 })
-export default class ProyectosPublicados {}
+export default class ProyectosPublicados {
+  private proyectoServicio = inject(ProyectoServicio);
+  private loginServicio = inject(LoginServicio);
+  private modalService = inject(ModalService);
+  protected proyectosActivos = signal<Proyecto[]>([]);
+  private idCliente = signal<number>(0);
+
+  ngOnInit() {
+    this.cargarProyectos();
+  }
+
+  private cargarProyectos() {
+    const usuario = this.loginServicio.getUsuario();
+    if (!usuario) {
+      return;
+    }
+    this.idCliente.set(usuario.id);
+
+    this.proyectoServicio.obtenerProyectosActivosPorCliente(this.idCliente()).subscribe({
+      next: (proyectos) => {
+        this.proyectosActivos.set(proyectos);
+      },
+      error: (error) => {
+        this.modalService.abrirError('Error al cargar los proyectos activos');
+      },
+    });
+  }
+}
