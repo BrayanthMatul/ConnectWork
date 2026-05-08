af5edab648e280c407e755330555dd8c8fc87ac7
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-07 21:42:31 -0600

    Eliminar componente SolictarHabilidad y su plantilla HTML

--- ConnectWorkAngular/src/app/components/Freelancer/SolictarHabilidad/SolictarHabilidad.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-solictar-habilidad',
-  imports: [],
-  templateUrl: './SolictarHabilidad.html',
-})
-export default class SolictarHabilidad {}
