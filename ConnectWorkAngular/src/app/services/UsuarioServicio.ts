044d4f9f3ed336e019df69df102298d588f287d3
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-10 15:55:45 -0600

    Agregar método obtenerUsuarioPorId en UsuarioServicio para obtener un usuario específico por su ID

--- ConnectWorkAngular/src/app/services/UsuarioServicio.ts
+++ ConnectWorkAngular/src/app/services/UsuarioServicio.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { Respuesta } from '../models/respuesta';
 import { Usuario } from '../models/usuario';
 import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
 
@@ -18,5 +19,9 @@ export class UsuarioServicio {
 
   public obtenerUsuarios(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(this.url);
+  }
+
+  public obtenerUsuarioPorId(id: number): Observable<Usuario | undefined> {
+    return this.obtenerUsuarios().pipe(map((usuarios) => usuarios.find((u) => u.id === id)));
   }
 }
