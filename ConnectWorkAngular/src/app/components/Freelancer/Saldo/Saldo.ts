dca7722609fe730fedc6922ea4ed2a5d8ab729aa
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-10 15:57:58 -0600

    Implementar la funcionalidad de saldo en el componente Saldo, incluyendo la carga y visualización del saldo del usuario.

--- ConnectWorkAngular/src/app/components/Freelancer/Saldo/Saldo.ts
+++ ConnectWorkAngular/src/app/components/Freelancer/Saldo/Saldo.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { PerfilServicio } from '../../../services/PerfilServicio';
+import { LoginServicio } from '../../../services/LoginServicio';
 
 @Component({
   selector: 'app-saldo',
   imports: [],
   templateUrl: './Saldo.html',
 })
-export default class Saldo {}
+export default class Saldo {
+  protected saldo = signal<number>(0);
+  private perfilServicio = inject(PerfilServicio);
+  private loginServicio = inject(LoginServicio);
+
+  ngOnInit() {
+    this.cargarSaldo();
+  }
+
+  private cargarSaldo() {
+    const usuario = this.loginServicio.getUsuario();
+    if (!usuario) {
+      console.error('No se pudo obtener el usuario logueado');
+      return;
+    }
+    this.perfilServicio.obtenerSaldoPerfil(usuario.id).subscribe({
+      next: (saldo) => {
+        this.saldo.set(saldo);
+      },
+      error: (error) => {
+        console.error('Error al obtener el saldo del perfil:', error);
+      },
+    });
+  }
+}
