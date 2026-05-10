ef90fb176c9c53683c8d886609561fc903bd86c4
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-10 15:54:56 -0600

    Agregar componente Postulacion con su lógica y plantilla HTML

---
+++ ConnectWorkAngular/src/app/components/Freelancer/Postulacion/Postulacion.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postulacion',
+  imports: [],
+  templateUrl: './Postulacion.html',
+})
+export default class Postulacion {}
