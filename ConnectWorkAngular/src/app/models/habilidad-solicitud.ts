5c2b73cb66ea60a66b8bf1831c48d4c500b70a72
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-11 11:50:51 -0600

    Agregar interfaz HabilidadSolicitud para gestionar solicitudes de habilidades

---
+++ ConnectWorkAngular/src/app/models/habilidad-solicitud.ts
@@ -0,0 +1,9 @@
+export interface HabilidadSolicitud {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  idCategoria: number;
+  idFreelancer: number;
+  aceptada: boolean;
+  revisada: boolean;
+}
