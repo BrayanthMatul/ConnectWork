e885bd341ed254e4d9f18c0e68702d54cd25c7fb
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-11 11:54:54 -0600

    Ajustar formato de importación y alinear propiedades en la interfaz Entrega

--- ConnectWorkAngular/src/app/models/entrega.ts
+++ ConnectWorkAngular/src/app/models/entrega.ts
@@ -1,11 +1,11 @@
-import { EstadoContrato } from "../enums/estado-contrato";
+import { EstadoContrato } from '../enums/estado-contrato';
 
 export interface Entrega {
-    id: number;
-    idContrato: number;
-    descripcion: string;
-    archivoUrl: string;
-    fechaHora: Date;
-    estado: EstadoContrato;
-    motivoRechazo: string;
+  id: number;
+  idContrato: number;
+  descripcion: string;
+  archivoUrl: string;
+  fechaHora: string;
+  estado: EstadoContrato;
+  motivoRechazo: string;
 }
