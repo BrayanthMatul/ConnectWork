daf6a27b265fdafa4f6cb81e01491d1de737140c
BrayanthMatul <brayanth.matul146@gmail.com>
2026-05-11 11:55:50 -0600

    Eliminar componente Solicitudes y su plantilla asociada

--- ConnectWorkAngular/src/app/components/Administrador/Categoria/Solicitudes/Solicitudes.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-solicitudes',
-  imports: [],
-  templateUrl: './Solicitudes.html',
-})
-export default class Solicitudes {}
